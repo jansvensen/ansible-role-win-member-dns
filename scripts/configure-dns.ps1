0333176e4bf9d6bc0b485d1f82c4ca96bc40d881
Sven Jansen <sven.jansen@devicetrust.com>
2023-06-07 11:06:34 +0200

    .

--- scripts/configure-dns.ps1
+++ scripts/configure-dns.ps1
@@ -4,9 +4,8 @@ param
     [Parameter(Mandatory=$True)][string]$dns_client_adapter
 )
 
-$DNSServerIP = $dns_client_dns_server_ip
 $IP = (Get-NetIPConfiguration -InterfaceAlias $dns_client_adapter).IPv4Address.IPAddress
 $Subnet = $IP.split(".")[0] + "." + $IP.split(".")[1] + "." + $IP.split(".")[2]+ "."
-$DNSServer = $Subnet + $DNSServerIP
+$DNSServer = $Subnet + $dns_client_dns_server_ip
 
 netsh interface ipv4 set dnsservers "Ethernet" static $DNSServer
